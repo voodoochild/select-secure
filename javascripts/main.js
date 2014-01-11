79fd3a788fcf033e0b4d2ec1c0ad3e94c09e5ab2
Kriss Watt <kriss.watt@gmail.com>
2014-01-11 16:53:41 -0500

    WIP -- filters styled

---
+++ javascripts/main.js
@@ -0,0 +1,19 @@
+/**
+ * Secure module
+ */
+var Secure = (function() {
+  function init() {
+
+  };
+
+  return {
+    init: init
+  };
+})();
+
+/**
+ * Initialise on page load
+ */
+$(function() {
+  Secure.init();
+});
